55a460cc69a10f033262adc97a63d9c343cf729b
Wojciech Kostecki <wt.kostecki@gmail.com>
2020-05-09 18:42:33 +0200

    Creating the ability to add new objects and search for them

--- src/main/java/pl/javastart/service/BookRepository.java
+++ src/main/java/pl/javastart/service/BookRepository.java
@@ -1,4 +1,37 @@
 package pl.javastart.service;
 
-public class BookRepository {
+import org.springframework.stereotype.Component;
+import pl.javastart.model.Book;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@Component
+public class BookRepository implements GenericRepository<String, Book> {
+    private List<Book> books;
+
+    public BookRepository() {
+        books = new LinkedList<>();
+    }
+
+    @Override
+    public Book get(String isbn) {
+        if (isbn == null || (isbn.length() != 13)) {
+            throw new IllegalArgumentException("Podaj prawidłowy numer ISBN");
+        }
+        Book find = books.stream()
+                .filter(b -> isbn.equals(b.getIsbn()))
+                .findFirst()
+                .get();
+        return find;
+    }
+
+    @Override
+    public void add(Book book){
+        if(book == null || book.getIsbn() == null || book.getTitle() == null || book.getAuthor() == null){
+            throw new IllegalArgumentException("Książka nie może miec pustych pól");
+        }
+        books.add(book);
+    }
 }
+
